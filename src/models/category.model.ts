6ed93586a18416a4e3dc31ac93ce33a829584c95
Gift <Giftibe62@gmail.com>
2023-04-04 15:56:31 +0100

    feat: created review model and updated the category model

--- src/models/category.model.ts
+++ src/models/category.model.ts
@@ -1,10 +1,13 @@
 import { model, Schema, } from 'mongoose'
 
 const categorySchema = new Schema({
-    name: {
+    name: [{
         type: String,
-    }
+        lowercase: true,
+        trim: true,
+        isDeleted: Boolean,
+        default: false
+    }]
 })
-
 const Category = model('category', categorySchema)
 export default Category
