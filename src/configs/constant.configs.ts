b562f00e93db8fd2875e995c57b2cd60dd65f1e0
Gift <Giftibe62@gmail.com>
2023-04-07 21:47:52 +0100

    feat:adding bcrypt password hashing

--- src/configs/constant.configs.ts
+++ src/configs/constant.configs.ts
@@ -4,21 +4,23 @@ const ENUM = {
     ADMIN: 'admin',
     SELLER: 'seller',
     BUYER: 'buyer',
-}
+};
 const MESSAGES = {
     DATABASE: {
-        CONNECTED: "MongoDB is connected :)",
-        ERROR: "An error occured while connecting to database "
+        CONNECTED: 'MongoDB is connected :)',
+        ERROR: 'An error occured while connecting to database ',
     },
 
     USER: {
-        CREATED: "User account created successfully",
-        ERROR: "An error occured",
-        DUPLICATE_EMAIL:"Email already exists",
-        DUPLICATE_USERNAME: "Username already exists",
-        REGISTERED: "Registration successful"
+        CREATED: 'User account created successfully',
+        ERROR: 'An error occured',
+        DUPLICATE_EMAIL: 'Email already exists',
+        DUPLICATE_USERNAME: 'Username already exists',
+        REGISTERED: 'Registration successful',
+        EMAIL_NOTFOUND: 'Email not found',
+        PASSWORD_NOTFOUND: 'Wrong password',
+        LOGIN: 'Login successful'
+    },
+};
 
-    }
-}
-
-export { PORT, ENUM, MESSAGES }
+export { PORT, ENUM, MESSAGES };
