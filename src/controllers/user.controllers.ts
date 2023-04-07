f3af3865e107187b522e1ba8e9f4f29d7fd0e671
Gift Ibe <57828233+giftibe@users.noreply.github.com>
2023-04-07 21:50:02 +0100

    Revert "feat:adding bcrypt password hashing"

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,7 +1,5 @@
 import { Request, Response } from "express";
 import UserServices from "../services/user.services";
-const ROUNDS = +process.env.SALT_ROUNDS!
-import encrypt from "../utils/hash.utils";
 import { MESSAGES } from "../configs/constant.configs";
 const { registerUser, findEmail, updateUser, deleteUser, findUserName } = new UserServices();
 
@@ -9,7 +7,6 @@ const { registerUser, findEmail, updateUser, deleteUser, findUserName } = new Us
 class userControllers {
     async registerUser(req: Request, res: Response) {
         try {
-            const { password } = req.body
             //find if email exists
             const getEmail = await findEmail(req.body.email)
             if (getEmail) {
@@ -31,17 +28,10 @@ class userControllers {
             }
 
             //Register a user 
-
-            const data = await encrypt(req.body.password, ROUNDS)
-            await registerUser({
-                password: data,
-                ...req.body
-            })
-
-            res.status(201).send({
-                success: true,
-                message: MESSAGES.USER.CREATED
-            })
+            const _user = registerUser(req.body);
+            res.status(201).send({ 
+                success: true, 
+                message: MESSAGES.USER.CREATED })
         } catch (error) {
             return res.status(500).send({
                 success: false,
@@ -49,73 +39,6 @@ class userControllers {
             })
         }
     }
-
-    async loginUser(req: Request, res: Response) {
-        try {
-            //check if email exist
-            const emailCheck = await findEmail(req.params.email)
-            if (!emailCheck) {
-                return res.status(403).send({
-                    success: false,
-                    message: MESSAGES.USER.EMAIL_NOTFOUND
-                })
-            }
-
-            //check if passwords match
-            if (emailCheck.password == req.body.password) {
-                //compare hashed password here then:
-                return res.status(403).send({
-                    success: false,
-                    message: MESSAGES.USER.PASSWORD_NOTFOUND
-                })
-            }
-
-            //Login in the user
-            //create cookie here then login
-
-            return res.status(200).send({
-                success: true,
-                message: MESSAGES.USER.LOGIN
-            })
-
-
-        } catch (error) {
-            return res.status(500).send({
-                success: false,
-                message: MESSAGES.USER.ERROR + error
-            })
-        }
-    }
-
-    async updateAccount(req: Request, res: Response) {
-        try {
-            const data = req.body
-            let account
-
-            /*check if logged in user is an admin or their email matches
-            the logged in email */
-            // const _user = await findEmail(account)
-
-            // const update = await updateUser()
-
-
-
-        } catch (error) {
-            return res.status(500).send({
-                success: false,
-                message: MESSAGES.USER.ERROR + error
-            })
-
-        }
-
-
-    }
-
-
-
-
 }
-
-
 
 export default new userControllers()
--- src/utils/hash.utils.ts
+++
@@ -1,6 +0,0 @@
-import bcrypt from "bcrypt"
-
-export default async function (password: string, saltRounds: number) {
-    const hash = await bcrypt.hash(password, saltRounds);
-    return hash.toString()
-}
