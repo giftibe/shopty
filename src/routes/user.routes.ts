42c545bd514afd35db370f497cef4162df63b451
Gift <Giftibe62@gmail.com>
2023-04-13 13:13:32 +0100

    feat: added the delete acount option

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,12 +2,19 @@ import express from 'express';
 const router = express.Router();
 import userControllers from '../controllers/user.controllers';
 import requireRole from '../middlewares/authorize.passport';
-const { registerAUser, loginUser, findAUsername, logoutUser } = userControllers
+const {
+    registerAUser,
+    loginUser,
+    findAUsername,
+    logoutUser,
+    deleteAccount
+} = userControllers
 
 
 router.post('/register', registerAUser)
 router.post('/login', loginUser)
 router.get('/find', requireRole('admin'), findAUsername)
 router.delete('/logout', logoutUser)
+router.delete('/delete', deleteAccount)
 
 export default router
